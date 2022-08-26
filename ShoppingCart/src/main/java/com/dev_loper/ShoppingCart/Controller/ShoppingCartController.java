3e2f01baf92e135231b37844f9afabb2de153bcc
devloper-ui <kbtidake@gmail.com>
2022-08-26 23:25:35 +0530

    Added code for Delete and Put for rest API

--- ShoppingCart/src/main/java/com/dev_loper/ShoppingCart/Controller/ShoppingCartController.java
+++ ShoppingCart/src/main/java/com/dev_loper/ShoppingCart/Controller/ShoppingCartController.java
@@ -7,9 +7,11 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -68,8 +70,22 @@ public class ShoppingCartController {
 	
 	@PostMapping("/list")
 	@ResponseBody
-	public Product addToCart(@RequestBody Product product) {	
+	public Product addToList(@RequestBody Product product) {	
 		cart.addProduct(product);
+		return product;
+	}
+	
+	@DeleteMapping("/list/{id}")
+	@ResponseBody
+	public void removeFromList(@PathVariable int id) {
+		//System.out.println(cart.removeProductById(id));
+		cart.removeProductById(id);
+	}
+	
+	@PutMapping("/list/{id}")
+	@ResponseBody
+	public Product updateFromList(@RequestBody Product product,@PathVariable int id) {
+		cart.updateProduct(product,id);
 		return product;
 	}
 }
