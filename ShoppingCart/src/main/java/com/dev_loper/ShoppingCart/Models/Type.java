b786bc03e4d0f6b67a38c8f324ee55e556de6245
devloper-ui <kbtidake@gmail.com>
2022-08-31 17:39:31 +0530

    Added Type class and its child classes in Product Class

--- ShoppingCart/src/main/java/com/dev_loper/ShoppingCart/Models/Type.java
+++ ShoppingCart/src/main/java/com/dev_loper/ShoppingCart/Models/Type.java
@@ -1,13 +1,5 @@
 package com.dev_loper.ShoppingCart.Models;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-
-@JsonTypeInfo (use = JsonTypeInfo.Id.NAME, include = As.PROPERTY, property = "typeName", visible=true)
-//picking up typename as a name for object to be created like smartphones, laptop etc;
-@JsonSubTypes ({@JsonSubTypes.Type (value = Smartphones.class, name = "Smartphones"), @JsonSubTypes.Type (value = Tshirts.class, name = "Tshirts"), @JsonSubTypes.Type (value = Washingmachines.class, name = "Washingmachines")})
-//name must be an exact match of child class names.
 public class Type {
 	
 	String typeName;
